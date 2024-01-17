ba45bc6b4af23c2fb2e375bfb81d5e1180b9abea
MeJlaHxOJluk <nana.u.mama99@gmail.com>
2024-01-17 13:40:16 +0300

    Used (try - cath).

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,5 +1,26 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Do you want to suck?");
+        try{
+            int res = calcukate();
+            System.out.println(res);
+            System.out.println("Do you want to suck?");
+        } catch (ArithmeticException exception){
+            System.out.println(exception);
+        }
+
+
+    }
+
+    private static int calcukate(){
+        var a = 1;
+        var b = 0;
+        int result = devided(a, b);
+
+        return result;
+    }
+
+    private static int devided(int x, int y){
+        var z = x /  y;;
+        return  z;
     }
 }
